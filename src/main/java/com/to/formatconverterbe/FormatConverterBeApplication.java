268c1d17b8c0f6d89f4b4383770a2e808dc05f16
Mvteo <mvteow@gmail.com>
2021-03-19 23:12:30 +0100

    create project

---
+++ src/main/java/com/to/formatconverterbe/FormatConverterBeApplication.java
@@ -0,0 +1,13 @@
+package com.to.formatconverterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatConverterBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormatConverterBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/to/formatconverterbe/FormatConverterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.to.formatconverterbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatConverterBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
